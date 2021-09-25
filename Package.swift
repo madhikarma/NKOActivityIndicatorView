af9bd20e8f035240dc46b0dede6f0661d1fb135d
Shagun Madhikarmi <madhikarma@gmail.com>
2021-09-25 14:57:49 +0100

    Update Package.swift to include platforms, products and targets

--- Package.swift
+++ Package.swift
@@ -3,5 +3,16 @@
 import PackageDescription
 
 let package = Package(
-	name: "NKOActivityIndicatorView"
+	name: "NKOActivityIndicatorView",
+	platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "NKOActivityIndicatorView",
+            targets: ["NKOActivityIndicatorView"]),
+    ],
+	targets: [
+        .target(
+           name: "NKOActivityIndicatorView",
+           path: "NKOActivityIndicatorView"), 
 )
